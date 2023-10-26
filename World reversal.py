3aae4f00c1997df801ce92e6ad0fdaf4c86223d9
Nicolasolgoso <145667874+Nicolasolgoso@users.noreply.github.com>
2023-10-26 20:30:55 +0200

    Update World reversal.py

--- World reversal.py
+++ World reversal.py
@@ -2,5 +2,5 @@ frase_input = str(input("Tu frase para invertir:"))
 frase_list = frase_input.split()
 print(frase_list)
 frase_list_reversed = frase_list.reverse()
-frase_output = frase_input
+
 print(frase_output)
